4a8551544b6041f94de021276cbdb924677f088f
Adam Niedzielski <adamsunday@gmail.com>
2014-05-10 17:04:22 +0200

    Add two serializers for ThemeGroup: - ThemeGroupSerializer - basic data when all groups are listed - ExtendedThemeGroupSerializer - includes weekly themes and users, use when only one group is rendered

---
+++ app/serializers/extended_theme_group_serializer.rb
@@ -0,0 +1,4 @@
+
+class ExtendedThemeGroupSerializer < ThemeGroupSerializer
+  has_many :weekly_themes
+end
---
+++ app/serializers/theme_group_serializer.rb
@@ -0,0 +1,3 @@
+class ThemeGroupSerializer < ActiveModel::Serializer
+  attributes :id, :name, :description
+end
