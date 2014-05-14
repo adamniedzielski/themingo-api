9dde73bd202a3dfb7c3c3bca0f7c95f92b20ef67
Adam Niedzielski <adamsunday@gmail.com>
2014-05-14 11:10:53 +0200

    Configure activeadmin resources

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,17 +1,16 @@
 ActiveAdmin.register User do
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+  permit_params :email,:encrypted_password, :reset_password_token, :reset_password_sent_at,
+                :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at,
+                :current_sign_in_ip, :last_sign_in_ip, :created_at, :updated_at,
+                :nickname, :first_name, :last_name, :description, :age, :location
+
+  index do
+    selectable_column
+    column :id
+    column :email
+    column :nickname
+    actions
+  end
   
 end
